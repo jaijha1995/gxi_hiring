17caff7daacd15fe9b8f654e4075d1421cd385a4
jaijha1995 <jaijhavats95@gmail.com>
2025-10-09 10:23:19 +0530

    New modify

--- restserver/settings.py
+++ restserver/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%r@^ik71zj1cy72)g)&zd4q$yb@wf)l%4%pm@l744fdt8!1nt@'
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '65.2.44.234']
 
 AUTH_USER_MODEL =  'superadmin.UserProfile'
 
