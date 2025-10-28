30dd1e48883e73ed26500181ea9e2a68b7c8013b
jaijha1995 <jaijhavats95@gmail.com>
2025-10-28 15:02:58 +0530

    N ewmodify

--- form_data/models.py
+++ form_data/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class FormData(models.Model):
     form_name = models.CharField(max_length=255 , default='gxi_form')
     submission_data = models.JSONField()
+    cv_upload = models.FileField(upload_to='cv_uploads/', blank=True, null=True)
     submitted_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
