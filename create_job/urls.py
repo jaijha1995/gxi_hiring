cb29967ba1b665298dd2cc7b2661ceac5990dc50
jaijha1995 <jaijhavats95@gmail.com>
2025-10-31 10:39:17 +0530

    Ne wmodfiy

--- create_job/urls.py
+++ create_job/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import SkillsAPIView, JobAPIView
-# from .departmentviews import DepartmentAPIView
-# from .jobtypesviews import jobtypesAPIView
+from .departmentviews import DepartmentAPIView
+from .jobtypesviews import jobtypesAPIView
 
 urlpatterns = [
     path('skills/', SkillsAPIView.as_view()),
@@ -10,10 +10,10 @@ urlpatterns = [
     path('jobs/', JobAPIView.as_view()),
     path('jobs/<int:pk>/', JobAPIView.as_view()),
 
-    # path('department/', DepartmentAPIView.as_view()),
-    # path('department/<int:pk>/', DepartmentAPIView.as_view()),
+    path('department/', DepartmentAPIView.as_view()),
+    path('department/<int:pk>/', DepartmentAPIView.as_view()),
 
 
-    # path('jobtypes/', jobtypesAPIView.as_view()),
-    # path('jobtypes/<int:pk>/', jobtypesAPIView.as_view()),
+    path('jobtypes/', jobtypesAPIView.as_view()),
+    path('jobtypes/<int:pk>/', jobtypesAPIView.as_view()),
 ]
