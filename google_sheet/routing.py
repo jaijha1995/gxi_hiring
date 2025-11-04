6c6846af57c13268866436fa2db10805b6dd4e4f
jaijha1995 <jaijhavats95@gmail.com>
2025-11-04 14:38:16 +0530

    Ne wmodify

--- google_sheet/routing.py
+++ google_sheet/routing.py
@@ -3,6 +3,7 @@ from .consumers import TypeformRealtimeConsumer
 from google_form_work.consumers import GoogleFormAllSheetsConsumer
 from form_data.consumer.consumers import FormDataRealtimeConsumer
 from form_data.consumer.consumers_realtime_model import FormDataModelRealtimeConsumer
+from tasks.consumers import TaskConsumer
 
 
 websocket_urlpatterns = [
@@ -10,4 +11,5 @@ websocket_urlpatterns = [
     re_path(r"ws/forms/$", GoogleFormAllSheetsConsumer.as_asgi()),
     re_path(r'^ws/formdata/$', FormDataRealtimeConsumer.as_asgi()),
     re_path(r'^ws/formdata_model/$', FormDataModelRealtimeConsumer.as_asgi()),
+    re_path(r"ws/tasks/$", TaskConsumer.as_asgi()),
 ]
